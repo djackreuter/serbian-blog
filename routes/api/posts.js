71430b9d51b9b46e7858dd0bfa49ff6f49ed800a
Dylan Reuter <djreuter45@gmail.com>
2018-10-01 13:04:47 -0600

    added get all posts and get post by id routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,8 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const { Post } = require('../../models/Post');
 
-router.get('/test', (req, res) => {
-  return res.send('posts working');
+/**
+ * @route  GET api/posts/
+ * @desc   get all posts
+ * @access public
+ */
+router.get('/', async (req, res) => {
+  let errors = {};
+  try {
+    const posts = await Post.find().populate('user', 'name');
+    if (!posts) {
+      errors.noposts = 'There are currently no posts';
+      return res.status(404).json(errors);
+    }
+    return res.json(posts);
+  } catch (err) {
+    return res.status(400).json(err);
+  }
+});
+
+/**
+ * @route  GET api/posts/:id
+ * @desc   get post by id
+ * @access public
+ */
+router.get('/:id', async (req, res) => {
+  errors = {};
+  try {
+    const post = await Post.findById(req.params.id).populate('user', 'name');
+    if (!post) {
+      errors.nopost = 'Post not found';
+      return res.status(404).json(errors);
+    }
+    return res.json(post);
+  } catch (err) {
+    return res.status(400).json(err);
+  }
 });
 
 module.exports = router;
