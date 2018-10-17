902cb76693741ff894d03210516f4fd6c7ee1008
Dylan Reuter <djreuter45@gmail.com>
2018-10-17 08:43:58 -0600

    added google passport auth strategy

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 
@@ -15,5 +16,23 @@ module.exports = (passport) => {
       }
       return done(null, false);
     }).catch((err) => console.log(err));
+  }));
+  passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: 'http://localhost:5000/api/users/auth/google/callback'
+  }, (accessToken, refreshToken, profile, callback) => {
+    User.findOne({ googleId: profile.id }).then((user) => {
+      if (user) {
+        return callback(null, user);
+      }
+      const newUser = new User({
+        name: profile.displayName,
+        googleId: profile.id
+      });
+      newUser.save().then((user) => {
+        return callback(null, user);
+      });
+    }).catch((err) => callback(err));
   }));
 };
