1079934f96ed100e2222f2613457ed7ced291c96
Dylan Reuter <djreuter45@gmail.com>
2018-10-23 13:21:31 -0600

    added basic routes

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <Link className="navbar-brand" to="/">Serbian Blog</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Features</a>
+              <Link className="nav-link" to="/posts">Posts</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Pricing</a>
+              <Link className="nav-link" to="/about">About</Link>
+            </li>
+          </ul>
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/register">Register</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link disabled" href="#">Disabled</a>
+              <Link className="nav-link" to="/login">Login</Link>
             </li>
           </ul>
         </div>
