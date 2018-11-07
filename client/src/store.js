0bfe565a5b75ae0a084a8ca366da6984667e2446
Dylan Reuter <djreuter45@gmail.com>
2018-11-07 12:01:07 -0600

    added support for redux devtools

--- client/src/store.js
+++ client/src/store.js
@@ -1,10 +1,17 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initialState = {};
 const middleware = [thunk];
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, initialState, applyMiddleware(...middleware));
+const store = createStore(
+  rootReducer, 
+  initialState,
+  composeEnhancers(
+    applyMiddleware(...middleware)
+  )
+);
 
 export default store;
