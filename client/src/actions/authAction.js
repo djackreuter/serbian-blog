330d5c104e8e9535dda52db37fa5decc2ae9ca21
Dylan Reuter <djreuter45@gmail.com>
2018-11-14 20:20:56 -0600

    started auth action

--- client/src/actions/authAction.js
+++ client/src/actions/authAction.js
@@ -1,4 +1,12 @@
-export const registerUser = (data) => ({
-  type: 'TEST_DISPATCH',
-  payload: data
-});
+import axios from 'axios';
+import { GET_ERRORS } from './types';
+
+export const registerUser = userData => dispatch => {
+  axios.post('/api/users/register', userData)
+    .then(res => console.log(res))
+    .catch(err => dispatch({
+      type: GET_ERRORS,
+      payload: err
+    })
+  );
+};
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -5,11 +5,7 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch(action.type) {
-    case 'TEST_DISPATCH':
-    return {
-      ...state,
-      user: action.payload
-    }
+
     default:
       return state;
   }
