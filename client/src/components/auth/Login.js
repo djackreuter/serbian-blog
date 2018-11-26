9f40bd36b5ee6731f5f6b38b331447fbdbfb4e1f
Dylan Reuter <djreuter45@gmail.com>
2018-11-26 14:22:41 -0600

    added errors to login form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -56,6 +56,7 @@ class Login extends Component {
                     value={this.state.email}
                     onChange={this.onChange}
                   />
+                  {errors.email && <div className="invalid-feedback">{errors.email}</div>}
                 </div>
                 <div className="form-group">
                   <input
@@ -68,6 +69,7 @@ class Login extends Component {
                     value={this.state.password}
                     onChange={this.onChange}
                   />
+                  {errors.password && <div className="invalid-feedback">{errors.password}</div>}
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
