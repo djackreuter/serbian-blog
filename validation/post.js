af1e2b050fd5ebcb4553706e47a95de238300c22
Dylan Reuter <djreuter45@gmail.com>
2018-11-26 14:21:36 -0600

    added post validation file

---
+++ validation/post.js
@@ -0,0 +1,21 @@
+const _ = require('lodash');
+const validator = require('validator');
+
+const validatePostInput = (data) => {
+  let errors = {};
+  data.title = !_.isEmpty(data.title) ? data.title : '';
+  data.body = !_.isEmpty(data.body) ? data.body : '';
+
+  if (validator.isEmpty(data.title)) {
+    errors.title = 'Title field is required';
+  }
+  if (validator.isEmpty(data.body) || validator.isLength(data.body, {min: 3})) {
+    errors.body = 'Body is not valid';
+  }
+  return {
+    errors,
+    isValid: _.isEmpty(errors)
+  }
+}
+
+module.exports = { validatePostInput }
