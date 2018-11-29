2040dcf69eb466d79987631e4250b30414095332
Dylan Reuter <djreuter45@gmail.com>
2018-11-29 10:13:36 -0600

    added redirect on success to register user action

--- client/src/actions/authAction.js
+++ client/src/actions/authAction.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import { GET_ERRORS } from './types';
 
-export const registerUser = userData => dispatch => {
+export const registerUser = (userData, history) => dispatch => {
+  console.log(userData);
   axios.post('/api/users/register', userData)
-    .then(res => console.log(res))
+    .then(res => history.push('/login'))
     .catch(err => dispatch({
       type: GET_ERRORS,
       payload: err.response.data
