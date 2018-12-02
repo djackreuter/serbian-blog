58a27103e4dbc6ffbf52206fc1f56769a41612fd
Dylan Reuter <djreuter45@gmail.com>
2018-12-02 13:27:53 -0600

    added login and set current user actions

--- client/src/actions/authAction.js
+++ client/src/actions/authAction.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import { GET_ERRORS } from './types';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
 
 export const registerUser = (userData, history) => dispatch => {
   console.log(userData);
@@ -14,10 +16,23 @@ export const registerUser = (userData, history) => dispatch => {
 
 export const loginUser = userData => dispatch => {
   axios.post('/api/users/login', userData)
-    .then(res => console.log(res))
+    .then(res => {
+      const { token } = res.data;
+      localStorage.setItem('jwtToken', token);
+      setAuthToken(token);
+      const decoded = jwt_decode(token);
+      dispatch(setCurrentUser(decoded))
+    })
     .catch(err => dispatch({
       type: GET_ERRORS,
       payload: err.response.data
     })
   );
 };
+
+export const setCurrentUser = (decoded) => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
+}
