f936ab3d8707b8c5d468946f1c99bd9bb613283d
Dylan Reuter <djreuter45@gmail.com>
2018-12-19 12:52:44 -0600

    added user action and reducer

---
+++ client/src/actions/userActions.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { GET_CURRENT_USER, CLEAR_CURRENT_USER, USER_LOADING, GET_ERRORS } from './types';
+
+export const getCurrentUser = () => dispatch => {
+  dispatch(setUserLoading());
+  axios.get('/api/users')
+    .then((res) => dispatch({
+      type: GET_CURRENT_USER,
+      payload: res.data
+      })
+    )
+    .catch((err) => dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    })
+  )
+}
+
+export const setUserLoading = () => {
+  return {
+    type: USER_LOADING
+  }
+}
+
+export const clearCurrentUser = () => {
+  return {
+    type: CLEAR_CURRENT_USER
+  }
+}
