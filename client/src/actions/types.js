286d2c3ed3292d922a2460947d26cac4b92b335f
Dylan Reuter <djreuter45@gmail.com>
2018-12-19 12:52:00 -0600

    added more types for new actions

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,2 +1,5 @@
 export const GET_ERRORS = 'GET_ERRORS';
 export const SET_CURRENT_USER = 'SET_CURRENT_USER';
+export const GET_CURRENT_USER = 'GET_CURRENT_USER';
+export const CLEAR_CURRENT_USER = 'CLEAR_CURRENT_USER';
+export const USER_LOADING = 'USER_LOADING';
