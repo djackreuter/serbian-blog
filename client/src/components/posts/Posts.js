e6fb9d67f5edd2d9af4527c9a47c9d9d385e1c5d
Dylan Reuter <djreuter45@gmail.com>
2019-01-21 14:52:23 -0600

    added post content to posts component

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,14 +3,28 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../common/spinner';
 import PostForm from './PostForm';
+import { getPosts } from '../../actions/postActions';
+import PostFeed from './PostFeed';
 
 class Posts extends Component {
+  componentDidMount() {
+    this.props.getPosts();
+  }
+
   render() {
+    const { posts, loading } = this.props.post;
+    let postContent;
+    if (posts == null || loading) {
+      postContent = <Spinner />
+    } else {
+      postContent = <PostFeed posts={posts} />
+    }
     return (
       <div className="container">
         <div className="row">
           <div className="col-md-12">
             <PostForm />
+            {postContent}
           </div>
         </div> 
       </div>
@@ -18,4 +32,13 @@ class Posts extends Component {
   }
 }
 
-export default Posts;
+Posts.propTypes = {
+  post: PropTypes.object.isRequired,
+  getPosts: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+  post: state.post
+});
+
+export default connect(mapStateToProps, { getPosts })(Posts);
