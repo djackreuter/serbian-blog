c8cb0c9da2d66054ffa2c8a533f12a90c5f0887c
Dylan Reuter <djreuter45@gmail.com>
2019-01-21 14:50:36 -0600

    added get posts and post loading actions and reducer cases

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { ADD_POST, GET_ERRORS } from './types';
+import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING } from './types';
 
 export const addPost = postData => dispatch => {
   axios.post('/api/posts', postData)
@@ -15,4 +15,27 @@ export const addPost = postData => dispatch => {
         payload: err.response.data
       })
     );
+}
+
+export const getPosts = () => dispatch => {
+  dispatch(postLoading());
+  axios.get('/api/posts')
+    .then(res => 
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data
+      })
+    )
+    .catch(err => 
+      dispatch({
+        type: GET_POSTS,
+        payload: null
+      })
+    )
+}
+
+export const postLoading = () => {
+  return {
+    type: POST_LOADING
+  }
 }
