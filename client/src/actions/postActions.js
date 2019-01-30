9b9955e8b20f8fc829a22f92357b468e58a05de0
Dylan Reuter <djreuter45@gmail.com>
2019-01-30 15:35:50 -0600

    added get post, deletepost and add like actions

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING } from './types';
+import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING, DELETE_POST, GET_POST } from './types';
 
 export const addPost = postData => dispatch => {
   axios.post('/api/posts', postData)
@@ -32,6 +32,52 @@ export const getPosts = () => dispatch => {
         payload: null
       })
     )
+}
+
+export const getPost = (id) => dispatch => {
+  dispatch(postLoading());
+  axios.get(`/api/posts/${id}`)
+    .then(res => 
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err => 
+      dispatch({
+        type: GET_POST,
+        payload: null
+      })
+    )
+}
+
+export const deletePost = id => dispatch => {
+  axios.delete(`/api/posts/${id}`)
+    .then(res => 
+      dispatch({
+        type: DELETE_POST,
+        payload: id
+      })
+    )
+    .catch(err => 
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+}
+
+export const addLike = id => dispatch => {
+  axios.post(`/api/posts/like/${id}`)
+    .then(res => 
+      dispatch(getPosts())
+    )
+    .catch(err => 
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
 }
 
 export const postLoading = () => {
