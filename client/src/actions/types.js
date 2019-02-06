8ab694ed233af5c3f6bf3b70c93e540ac3c9697b
Dylan Reuter <djreuter45@gmail.com>
2019-02-06 15:43:10 -0600

    added add comment type

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -8,3 +8,4 @@ export const GET_POSTS = 'GET_POSTS';
 export const GET_POST = 'GET_POST';
 export const DELETE_POST = 'DELETE_POST';
 export const POST_LOADING = 'POST_LOADING';
+export const ADD_COMMENT = 'ADD_COMMENT';
