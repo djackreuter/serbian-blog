82715178bec5a9a82305d7f58836fa546ceb2c0c
Dylan Reuter <djreuter45@gmail.com>
2019-02-13 16:01:05 -0600

    removed google types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -8,6 +8,3 @@ export const GET_POSTS = 'GET_POSTS';
 export const GET_POST = 'GET_POST';
 export const DELETE_POST = 'DELETE_POST';
 export const POST_LOADING = 'POST_LOADING';
-export const ADD_COMMENT = 'ADD_COMMENT';
-export const SET_GOOGLE_USER = 'SET_GOOGLE_USER';
-export const CLEAR_GOOGLE_USER = 'CLEAR_GOOGLE_USER';
