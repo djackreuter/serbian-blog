4c5ad080f611e50bdf702d1a50cb63618ec94d3f
Dylan Reuter <djreuter45@gmail.com>
2019-02-26 21:22:18 -0600

    alignment fix

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -23,7 +23,7 @@ class About extends Component {
           )}
           <p className="lead"><strong>{admin.name}</strong></p>
           <div className="row">
-            <div className="col-md-10">
+            <div className="col-md-12">
               <p>{admin.bio}</p>
             </div>
           </div>
